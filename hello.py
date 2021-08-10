edf87408b764a47cc1b49f6e9ce6cea6e6870f95
fishwangx <yu@taifanmagellan.com>
2021-08-10 13:50:38 +0800

    updated requirements.txt

---
+++ hello.py
@@ -0,0 +1,9 @@
+def hello():
+
+    return "hi"
+
+
+def add(x,y):
+
+    return x+y
+
---
+++ test_hello.py
@@ -0,0 +1,9 @@
+from hello import add
+
+
+def test_add():
+
+
+    assert add(2,3)==5
+
+    assert add(6,7)==13
