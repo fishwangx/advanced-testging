505392ca9da8115cef3d23d4d611051bb39d8c0d
fishwangx <yu@taifanmagellan.com>
2021-08-10 14:18:22 +0800

    added multiply

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,7 @@
 from hello import add
 
+from multi import multiply
+
 
 def test_add():
 
@@ -7,3 +9,8 @@ def test_add():
     assert add(2,3)==5
 
     assert add(6,7)==13
+
+
+def test_multi():
+
+    assert multiply(2,3)==6
